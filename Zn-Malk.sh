adb957692e48f10d975487df6ad4d772d9cca63a
ZnMalk <55311955+ZnMalk@users.noreply.github.com>
2019-09-14 15:51:37 +0700

    Add files via upload

---
+++ Zn-Malk.sh
@@ -0,0 +1,42 @@
+clear
+#!/bin/sh
+
+sleep 3
+
+echo '\033[0;32m /~~~~~~~~~~~~~~~~~~~~~~~~~\'
+echo '\033[1;33m |       By:Zn-Malk        |'
+echo '\033[1;33m |       Versi Beta        |'
+echo '\033[0;32m \~~~~~~~~~~~~~~~~~~~~~~~~~/'
+sleep 1
+echo '\033[0;31mwait...'
+sleep 5
+echo '\033[0;34mStarting Tools...'
+sleep 3
+read -p 'Enter  Your Name : ' Name1
+echo '\033[0;32m Wellcome To My Tools Mr' $Name1
+sleep 10
+clear
+echo '\033[0;31m This Is My Tools!!!'
+sleep 3
+clear
+sleep 3
+echo '\033[1;31m Hello Mr' $Name1
+sleep 3
+echo '\033[0;36m|=======================================|'
+echo '\033[0;36m|  _____            __  __       _ _    |'
+echo '\033[0;36m| |__  /_ __  ZN7M |  \/  | __ _| | | __|'
+echo '\033[0;36m|   / /|  _  \_____| |\/| |/ _` | | |/ /|'
+echo '\033[0;36m|  / /_| | | |_____| |  | | (_| | |   < |'
+echo '\033[0;36m| /____|_| |_|     |_|  |_|\__,_|_|_|\_\|'
+echo '\033[0;36m|=======================================|'
+echo 'Loading...'
+sleep 7
+echo 'Wait 7 Second...'
+
+echo 'Script nya belum Jadi Goblok! Ngapa Lu InstallxD'
+
+echo 'Bye Bye!!! XD'
+
+sleep 3
+
+exit
